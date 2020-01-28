80971ef300a03803ec87f8cbde8066d3a8d1c299
TinLattNandar <32725880+TinLattNandar@users.noreply.github.com>
2020-01-28 19:10:06 +0630

    Update newJewelsInStones.java

--- newJewelsInStones.java
+++ newJewelsInStones.java
@@ -1,10 +1,6 @@
-You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
-Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+You are given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels. The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
-The letters in J are guaranteed distinct, and all characters in J and S are letters. 
-Letters are case sensitive, so "a" is considered a different type of stone from "A".
-
-#Brute-Force Approach 
+Brute-Force Approach 
 - Just pointing out each element whether it is match.
 
 ########
@@ -25,5 +21,6 @@ class Solution {
     return count;        
     }
 }
+
 #######
 Run Time - 0ms
