ecbc803ab8d1a2148ff2799e11338d7c5f43a8ab
TinLattNandar <32725880+TinLattNandar@users.noreply.github.com>
2020-01-28 19:06:57 +0630

    Create newJewelsInStones.java

---
+++ newJewelsInStones.java
@@ -0,0 +1,32 @@
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. 
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+#Brute-Force Approach 
+- Just pointing out each element whether it is match.
+
+########
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+
+    int count = 0;
+    int Slength = S.length();
+    int Jlength = J.length();
+        
+    for(int i = 0; i < Slength; i++){
+        for(int j= 0; j< Jlength; j++){
+            if(J.charAt(j) == S.charAt(i)){
+                count++;
+                break;
+            }            
+        } 
+    }
+        
+    return count;
+        
+    }
+}
+#######
+Run Time - 0ms
